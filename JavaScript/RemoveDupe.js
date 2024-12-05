81d8d5cfebd195cac7a1928871a7139d6e836eb1
adrienner1988 <adriennerdaniels@gmail.com>
2024-12-05 15:13:11 -0500

    "Removing dupes"

--- JavaScript/RemoveDupe.js
+++ JavaScript/RemoveDupe.js
@@ -5,3 +5,22 @@
 // console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Expected output: [1, 2, 3, 4, 5]
 // console.log(removeDuplicates(["apple", "banana", "apple"])); // Expected output: ["apple", "banana"]
 // console.log(removeDuplicates([])); // Expected output: []
+
+// Input array
+// loop through array
+// check if dupe
+// If dupe remove
+// Output new array, dupes removed
+
+const removeDuplicates = (array) => {
+    return array.reduce((accumulator, currentValue) => {
+        if (!accumulator.includes(currentValue)) {
+            accumulator.push(currentValue)
+        }
+        return accumulator
+    }, [])
+    }
+
+    console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+      
+    
