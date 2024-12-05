b5b8b2187bce36960b4fcd1cdb9d8d501cbe9a79
adrienner1988 <adriennerdaniels@gmail.com>
2024-12-04 22:13:33 -0500

    reverse array

--- JavaScript/ReverseArray.js
+++ JavaScript/ReverseArray.js
@@ -1,8 +1,32 @@
 // Write a function that takes an array and returns it reversed.
 
-// javascript
-// Copy code
 // // Test Cases
 // console.log(reverseArray([1, 2, 3, 4, 5])); // Expected output: [5, 4, 3, 2, 1]
 // console.log(reverseArray(["a", "b", "c"])); // Expected output: ["c", "b", "a"]
 // console.log(reverseArray([])); // Expected output: []
+
+// Input array
+// Reverse method
+// for loop starting at end to reverse
+// Output array in reverse
+
+// Reverse Method
+const reverseArray = (array) => {
+    return array.reverse()
+}
+
+// For loop
+const reverseArray2 = (array) => {
+    let newArray = [];
+
+    for (let i = array.length -1; i >= 0; i--) {
+        newArray.push(array[i]);
+    }
+    return newArray
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(reverseArray([]));
+
+console.log(reverseArray2([1, 2, 3, 4, 5]));
+console.log(reverseArray2([]));
